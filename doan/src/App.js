ef97afee8f1845a5972d6223e66afa7631325491
TruongNgan6 <truongvokimngan13@gmail.com>
2024-03-29 11:05:35 +0700

    first commit

---
+++ doan/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
